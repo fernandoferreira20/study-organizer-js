15941be790130381c89f4ef1c72b9489113c5bd1
Fernando Ferreira <fernandojrferreira@outlook.com>
2026-07-08 16:38:33 +0200

    Initialize JavaScript application

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// ======================================
+// IMPORTS
+// ======================================
+
+// More modules will be imported here later.
+
+
+// ======================================
+// APP INITIALIZATION
+// ======================================
+
+function initializeApp() {
+  console.log("Study Organizer JS is running.");
+}
+
+
+// ======================================
+// START APP
+// ======================================
+
+initializeApp();
